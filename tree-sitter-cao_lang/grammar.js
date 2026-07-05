39a9eb65f9483405658795b149793daabd93efac
Daniel Kiss <littlesnorrboy@gmail.com>
2026-07-05 11:55:50 +0200

    Add some initial grammar rules

--- tree-sitter-cao_lang/grammar.js
+++ tree-sitter-cao_lang/grammar.js
@@ -12,6 +12,37 @@ export default grammar({
 
   rules: {
     // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+    source_file: ($) => repeat($._definition),
+    _definition: ($) =>
+      choice(
+        $.function_definition,
+        // TODO: imports, submodules
+      ),
+    function_definition: ($) =>
+      seq("fn", $.identifier, $.parameter_list, $.block),
+    parameter_list: ($) =>
+      seq(
+        "(",
+        // TODO: parameters
+        ")",
+      ),
+
+    block: ($) => seq("{", repeat($._statement), "}"),
+    _statement: ($) =>
+      choice(
+        $.return_statement,
+        // TODO: other kinds of statements
+      ),
+    return_statement: ($) => seq("return", $.expression, ";"),
+    expression: ($) =>
+      choice(
+        $.identifier,
+        $.integer,
+        // TODO: other kinds of expressions
+      ),
+
+    identifier: ($) => /[a-z]+/,
+
+    integer: ($) => /\d+/,
+  },
 });
