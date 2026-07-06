5ec50e7d78b441cb106118c5a01fac8d8aba75bc
Daniel Kiss <littlesnorrboy@gmail.com>
2026-07-06 15:49:05 +0200

    Use clap_derive in disassembler

--- disassembler/src/main.rs
+++ disassembler/src/main.rs
@@ -1,21 +1,28 @@
 use cao_lang::{compiler, prelude::CaoProgram};
-use clap::{Arg, Command};
+use clap::Parser;
+
+#[derive(Debug, clap_derive::Parser)]
+struct Args {
+    #[arg()]
+    format: Format,
+}
+
+#[derive(Debug, clap_derive::ValueEnum, Clone, Default)]
+enum Format {
+    #[default]
+    Json,
+}
 
 fn main() {
-    let app = Command::new("Cao-Lang Disassembler").arg(
-        Arg::new("json")
-            .long("json")
-            .help("Accept json encoded cao-lang program"),
-    );
+    let args = Args::parse();
 
-    let args = app.get_matches();
-    if args.get_flag("json") {
-        let reader = std::io::BufReader::new(std::io::stdin().lock());
-        let pl: CaoProgram = serde_json::from_reader(reader).expect("Failed to deserialize");
-        let compiled = compiler::compile(pl, None).expect("Failed to compile");
+    match args.format {
+        Format::Json => {
+            let reader = std::io::BufReader::new(std::io::stdin().lock());
+            let pl: CaoProgram = serde_json::from_reader(reader).expect("Failed to deserialize");
+            let compiled = compiler::compile(pl, None).expect("Failed to compile");
 
-        compiled.print_disassembly();
-    } else {
-        panic!("Missing format")
+            compiled.print_disassembly();
+        }
     }
 }
